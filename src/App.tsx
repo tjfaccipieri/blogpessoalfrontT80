6a0e257a1638238501e6d5815705bfbd40a9bfd2
Thiago Faccipieri <thiago.faccipieri@gmail.com>
2025-04-08 07:34:07 -0300

    Front até models

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Home from './pages/home/Home';
+import Cadastro from './pages/cadastro/Cadastro';
+import Login from './pages/login/Login';
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <div className="min-h-[80vh]">
+          <Routes>
+          <Route path="/" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+              <Route path="/login" element={<Login />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
